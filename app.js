f8f66ba3cc97f57611c7a1e4231547c66cd1a917
Charbel Bou Ghazalé <98774044+CharbelBG@users.noreply.github.com>
2022-07-10 17:54:42 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,35 +1,55 @@
-   /**to do
-     * both have turns, and they switch after one move
-     * each round, we check the number of cells that are empty, and we check any winning patterns
-     * created an array from 1 to 9, each move we do, we splice form the array
-     * create an annouce function that will display the winner in case there is one
-     * adding the display winnner
-     */
 (function(){
     let mainMenu = document.querySelector(".mainMenu");
     let gameSection = document.querySelector(".gameSection");
-    let start_button = document.querySelector(".mainContainer .button");
-    let currentPlayer = "❌";
-    let displayCurrentPlayer = document.querySelector(".currentPlayer");
-    displayCurrentPlayer.innerText = `Current player: ❌`;
-    let board = ['','','','','','','','','']; //that array will be mapped live by the game output.
+    let start_button = document.querySelector("#start_button");
+    let endGameButton = document.querySelector("#endGame_button");
+    let board = ["","","","","","","","",""];
     let isGameRunning = true;
-    let displayWinnerText = document.querySelector(".announcer"); 
+    let currentPlayer = "X";
+    let displayText = document.querySelector(".displayText");
+
+    displayText.innerText = `Current Player:X`;
     start_button.onclick = () =>{
-            mainMenu.classList.remove("visible");
-            mainMenu.classList.add("hidden");
-            gameSection.classList.remove("hidden");
-            gameSection.classList.add("visible");
+        mainMenu.classList.remove("visible");
+        mainMenu.classList.add("hidden");
+        gameSection.classList.remove("hidden");
+        gameSection.classList.add("visible");
     }
-    let end_button = document.querySelector(".gameSection .button");
-    end_button.onclick = () =>{
-        isGameRunning = true;
-        currentPlayer = "❌";
-        resetBoard();
+    endGameButton.onclick = () =>{
         gameSection.classList.remove("visible");
         gameSection.classList.add("hidden");
         mainMenu.classList.remove("hidden");
         mainMenu.classList.add("visible");
+        reset();
+    }
+    let tiles = Array.from(document.querySelectorAll(".gridItem"));
+    tiles.forEach((tile, index)=> {
+        tile.addEventListener("click",() => userAction(tile,index));
+    });
+    function validAction(tile){
+        if(tile.innerText ==""){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    function changePlayer(){
+        if(!isGameRunning){
+            return;
+        }
+        if(currentPlayer == "X"){
+            currentPlayer = "O";
+            displayText.innerText = `Current Player:${currentPlayer}`;
+        }
+        else{
+            currentPlayer = "X";
+            displayText.innerText = `Current Player:${currentPlayer}`;
+        }
+    }
+    function applyToTile(tile,index){
+        tile.innerText = currentPlayer;
+        board[index] = currentPlayer;
+        addColors(tile);
     }
     const winningConditions = [
         [0,1,2],
@@ -41,81 +61,72 @@
         [0,4,8],
         [2,4,6]
     ];
-    const tiles = Array.from(document.querySelectorAll(".gridElement"));
-    tiles.forEach((tile, index) =>{
-        tile.addEventListener("click",()=> userAction(tile,index));
-    });
-    function playerChange(){
-        if(currentPlayer == "❌"){
-            currentPlayer = "⭕";
-            displayCurrentPlayer.innerText = `Current player: ${currentPlayer}`;
-        }else{
-            currentPlayer = "❌";
-            displayCurrentPlayer.innerText = `Current player: ${currentPlayer}`;
-        }
-    }
-    function isValidIndex(tile){
-        if(tile.innerText !==''){
-            return false;
-        }
-        else{
-            return true;
-        }
-    }
-    function updateBoard(index){
-        board[index] = currentPlayer;
-    }
-    function resetBoard(){
-        board = ['','','','','','','','',''];
-        tiles.forEach((tile)=>{
-            tile.innerText = "";
-        });
-        displayWinnerText.innerText = "";
-        currentPlayer = "❌";
-        displayCurrentPlayer.innerText = "Current player: ❌";
-    }
-    function didAnyOneWin(){
-        //see if any winning pattern is applicable, or if it is a tie.
-        let roundWin = false;
-        for(let i = 0 ; i<=7 ; i++){
-            const winCondition = winningConditions[i]; //iterating an array of arrays.
-            let a = board[winCondition[0]];
-            let b = board[winCondition[1]];
-            let c = board[winCondition[2]];
-            if(a == "" || b == "" || c == ""){
-                console.log("cheking code is runnning");
-                continue; //skip iteration
-            }
-            if(a === b && b ===c){
-                 //stop any further inputs.
-                roundWin = true;
-                break;
-            }
-        }
-        if(roundWin){
-            displayWinner(currentPlayer);
-            isGameRunning = false;
-            console.log("winner must be annouced");
-        }
-        if (!board.includes('')) //there are no more empty spaces.
-        displayWinner("TIE");
-    }
-    //utility function that displays the winner
-    function displayWinner(player){
-        if(player == "TIE"){
-            displayWinnerText.innerText = "TIE!"
-        }
-        else{
-            displayWinnerText.innerText = `the winner is player${player}`;
-        }
-    }
 
-    function userAction(tile, index){
-        if(isValidIndex(tile) && isGameRunning){
-           tile.innerText = currentPlayer;
-           updateBoard(index);
-           didAnyOneWin();     
-           playerChange(); //must be called at the end
+    function didAnyOneWin(){
+        let roundWon = false;
+        for(let i = 0 ; i <=7;i++){
+            const winningCondition = winningConditions[i];
+            const a = board[winningCondition[0]];
+            const b = board[winningCondition[1]];
+            const c = board[winningCondition[2]];
+            if(a == "" || b == "" || c ==""){
+                continue;
+            }
+            if(a===b && b===c){
+                roundWon = true;
+                highLightWinningPath(winningCondition);
+                    break;
+            }
         }
+        if(roundWon){
+            isGameRunning = false;
+            displayWinner(currentPlayer);
+        }
+        if(!roundWon && !board.includes("")){
+            isGameRunning = false;
+            displayText.innerText = `TIE!`;
+        }
+    }
+    function displayWinner(value){
+        displayText.innerText = `${value} is the winner!`;
+    }
+    function userAction(tile,index){
+        if(validAction(tile)&&isGameRunning){
+            
+            applyToTile(tile,index);
+            didAnyOneWin();
+            changePlayer();
+        }
+    }
+    function reset(){
+        tiles.forEach((tile)=>{
+            tile.innerHTML = "<button></button>";
+            tile.classList.remove("limeColor");
+            tile.classList.remove("cyanColor");
+            tile.classList.remove("orangeColor");
+        });
+        isGameRunning = true;
+        currentPlayer = "X";
+        displayText.innerText = `Current Player:X`;
+        board = ["","","","","","","","",""];
+    }
+    function highLightWinningPath(path){
+        tiles.forEach((tile,index)=>{
+            for(let i =0; i<3; i++){
+                if(index == path[i]){
+                    console.log("index "+index);
+                    tile.classList.remove("cyanColor");
+                    tile.classList.remove("orangeColor");
+                    tile.classList.add("limeColor");
+                }
+            }
+        });
+    }
+    function addColors(tile){
+        if(tile.innerText == "X"){
+            tile.classList.add("cyanColor");
+        }else{
+            tile.classList.add("orangeColor");
+        } 
     }
 }());
